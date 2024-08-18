21077ec62379d6c6b71780b2ea3862d82a6dde65
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-08-18 16:39:37 +0000

    add header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,13 @@ import { PlaygroundComponent } from './playground/playground.component';
 
 export const routes: Routes = [
     {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'playground'
+    },
+    {
         path: 'playground',
+        title: 'Playground',
         component: PlaygroundComponent
     }
 ];
--- src/app/scroll-up-button/scroll-up-button.component.ts
+++ src/app/scroll-up-button/scroll-up-button.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { WINDOW, WINDOW_PROVIDERS } from '../core/service/window.service';
 
 @Component({
   selector: 'app-scroll-up-button',
   standalone: true,
   imports: [],
+  providers: [WINDOW_PROVIDERS],
   templateUrl: './scroll-up-button.component.html',
   styleUrl: './scroll-up-button.component.scss'
 })
 export class ScrollUpButtonComponent {
+
+  constructor(@Inject(WINDOW) private window: Window,) {
+  }
+
+  scrollToTop() {
+    this.window.scrollTo(0, 0);
+  }
 
 }
