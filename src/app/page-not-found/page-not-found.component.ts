7c5e53d2c5c15e50cbc4693b5ee7757f4b53b175
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-09-24 22:49:54 +0700

    not found page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ImageSrcsetPipe } from "../core/pipe/image-srcset.pipe";
+import { ReflectionFontComponent } from "../shared/reflection-font/reflection-font.component";
+import { MoreButtonComponent } from "../shared/more-button/more-button.component";
+import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [ImageSrcsetPipe, ReflectionFontComponent, MoreButtonComponent, RouterLink, TranslateModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
