0add09cd65b1782a81ecfe27740deb191fac7ff5
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-10-06 11:33:53 +0700

    console log frame height

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AutoAdjustIframeHeightWidthDirective } from '../shared/auto-adjust-iframe-height-width.directive';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [AutoAdjustIframeHeightWidthDirective],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
---
+++ src/app/shared/auto-adjust-iframe-height-width.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AutoAdjustIframeHeightWidthDirective } from './auto-adjust-iframe-height-width.directive';
+
+describe('AutoAdjustIframeHeightWidthDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AutoAdjustIframeHeightWidthDirective();
+    expect(directive).toBeTruthy();
+  });
+});
