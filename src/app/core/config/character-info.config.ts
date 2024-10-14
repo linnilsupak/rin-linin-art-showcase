fba76f6ef36d479989446f15ae5c4c48bc1645ce
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-10-14 11:35:46 +0700

    refactor to webp

--- src/app/core/config/character-info.config.ts
+++ src/app/core/config/character-info.config.ts
@@ -1,26 +1,26 @@
 export const characterInfo = {
     linin: {
-        imageUrl: '/assets/images/original/rin_linin_ori_preview.jpeg',
+        imageUrl: '/assets/images/original/rin_linin_ori_preview.webp',
         title: 'ORIGINAL.LININ.TITLE',
         description: 'ORIGINAL.LININ.DESCRIPTION',
     },
     collector: {
-        imageUrl: '/assets/images/original/the_collector_ori_rin_linin_preview.jpeg',
+        imageUrl: '/assets/images/original/the_collector_ori_rin_linin_preview.webp',
         title: 'ORIGINAL.COLLECTOR.TITLE',
         description: 'ORIGINAL.COLLECTOR.DESCRIPTION'
     },
     witch: {
-        imageUrl: '/assets/images/original/the_witch_ori_rin_linin_preview.jpeg',
+        imageUrl: '/assets/images/original/the_witch_ori_rin_linin_preview.webp',
         title: 'ORIGINAL.THE_WITCH.TITLE',
         description: 'ORIGINAL.THE_WITCH.DESCRIPTION'
     },
     francois: {
-        imageUrl: '/assets/images/original/francois_ori_rin_linin_preview.jpeg',
+        imageUrl: '/assets/images/original/francois_ori_rin_linin_preview.webp',
         title: 'ORIGINAL.FRANCOIS.TITLE',
         description: 'ORIGINAL.FRANCOIS.DESCRIPTION'
     },
     elysia: {
-        imageUrl: '/assets/images/original/elysia_ori_rin_linin_preview.jpeg',
+        imageUrl: '/assets/images/original/elysia_ori_rin_linin_preview.webp',
         title: 'ORIGINAL.ELYSIA.TITLE',
         description: 'ORIGINAL.ELYSIA.DESCRIPTION'
     },
