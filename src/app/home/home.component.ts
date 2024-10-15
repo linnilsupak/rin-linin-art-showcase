61d29d80a3bc6756d4eac88a37f358bdd926f877
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-10-15 11:57:27 +0700

    show case

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { ReflectionFontComponent } from '../shared/reflection-font/reflection-font.component';
 import { ImageCardComponent } from "../shared/image-card/image-card.component";
+import { CharacterIconComponent } from "../shared/character-icon/character-icon.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TranslateModule, ReflectionFontComponent, ImageCardComponent],
+  imports: [TranslateModule, ReflectionFontComponent, ImageCardComponent, CharacterIconComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
