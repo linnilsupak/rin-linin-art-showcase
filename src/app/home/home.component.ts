be347d2be0c5382a63f146c3db405076d57c27cd
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-10-15 08:56:47 +0700

    add door to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { ReflectionFontComponent } from '../shared/reflection-font/reflection-font.component';
+import { ImageCardComponent } from "../shared/image-card/image-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TranslateModule, ReflectionFontComponent],
+  imports: [TranslateModule, ReflectionFontComponent, ImageCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/shared/image-card/image-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { ReflectionFontComponent } from "../reflection-font/reflection-font.component";
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-image-card',
+  standalone: true,
+  imports: [ReflectionFontComponent, CommonModule],
+  templateUrl: './image-card.component.html',
+  styleUrl: './image-card.component.scss'
+})
+export class ImageCardComponent {
+  @Input() imageUrl: string;
+  @Input() title: string;
+  @Input() miniTitle: string;
+  @Input() link: string;
+
+}
