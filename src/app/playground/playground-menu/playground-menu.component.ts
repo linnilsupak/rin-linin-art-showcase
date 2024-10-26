41d221fdf0795a5489826d55761face1eda15d0e
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-10-26 10:19:26 +0000

    playground main menu

---
+++ src/app/playground/playground-menu/playground-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-playground-menu',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './playground-menu.component.html',
+  styleUrl: './playground-menu.component.scss'
+})
+export class PlaygroundMenuComponent {
+  @Input() label: string;
+
+}
--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -3,11 +3,14 @@ import { TranslateModule } from '@ngx-translate/core';
 import { playgroundGalleryConfig } from '../core/config/playground.config';
 import { GalleryRoomComponent } from "../shared/gallery-room/gallery-room.component";
 import { ReflectionFontComponent } from "../shared/reflection-font/reflection-font.component";
+import { ImageCardComponent } from "../shared/image-card/image-card.component";
+import { CharacterIconComponent } from "../shared/character-icon/character-icon.component";
+import { PlaygroundMenuComponent } from "./playground-menu/playground-menu.component";
 
 @Component({
   selector: 'app-playground',
   standalone: true,
-  imports: [ReflectionFontComponent, TranslateModule, GalleryRoomComponent],
+  imports: [ReflectionFontComponent, TranslateModule, GalleryRoomComponent, ImageCardComponent, CharacterIconComponent, PlaygroundMenuComponent],
   templateUrl: './playground.component.html',
   styleUrl: './playground.component.scss'
 })
