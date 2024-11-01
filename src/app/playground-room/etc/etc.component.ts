9a63072677308ff74cf159fbb12785d0fced4777
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-11-01 23:38:22 +0700

    etc room

--- src/app/playground-room/etc/etc.component.ts
+++ src/app/playground-room/etc/etc.component.ts
@@ -13,5 +13,192 @@ import { PlaygroundBottomMenuComponent } from '../playground-bottom-menu/playgro
   styleUrl: './etc.component.scss'
 })
 export class EtcComponent {
+  etc1 = [
+    {
+      picUrl: '/assets/images/playground/etc/remade_2024_rin_linin.webp',
+      frameStyle: 'black-frame',
+      content: {
+        title: 'Rin Linin: Flower room',
+        title2: 'Design and draw by Rin Linin',
+        subTitle: 'watercolor',
+        description: 'I visit Team Lab at Japan and the flower room kind of made me want to draw this picture.'
+      },
+      previewPic: {
+        width: 'inherit',
+        height: 'inherit',
+        disabled: true
+      }
+    },
+    {
+      picUrl: '/assets/images/playground/etc/uzbekistan_rin_linin.webp',
+      frameStyle: 'black-frame',
+      content: {
+        title: 'Rin Linin: Flower room',
+        title2: 'Design and draw by Rin Linin',
+        subTitle: 'watercolor',
+        description: 'I visit Team Lab at Japan and the flower room kind of made me want to draw this picture.'
+      },
+      previewPic: {
+        width: 'inherit',
+        height: 'inherit',
+        disabled: true
+      }
+    },
+    {
+      picUrl: '/assets/images/playground/etc/novel_cover_knight_rin_linin.webp',
+      frameStyle: 'black-frame',
+      content: {
+        title: 'Rin Linin: Flower room',
+        title2: 'Design and draw by Rin Linin',
+        subTitle: 'watercolor',
+        description: 'I visit Team Lab at Japan and the flower room kind of made me want to draw this picture.'
+      },
+      previewPic: {
+        width: 'inherit',
+        height: 'inherit',
+        disabled: true
+      }
+    },
+  ];
+  etc2 = [
+    {
+      picUrl: '/assets/images/playground/etc/tobias_rin_linin.webp',
+      frameStyle: 'black-frame circle small',
+      content: {
+        title: 'Rin Linin: Flower room',
+        title2: 'Design and draw by Rin Linin',
+        subTitle: 'watercolor',
+        description: 'I visit Team Lab at Japan and the flower room kind of made me want to draw this picture.'
+      },
+      previewPic: {
+        width: 'inherit',
+        height: 'inherit',
+        disabled: true
+      }
+    },
+    {
+      picUrl: '/assets/images/playground/etc/fiona_first_draft_rin_linin.webp',
+      frameStyle: 'white-frame circle small',
+      content: {
+        title: 'Rin Linin: Flower room',
+        title2: 'Design and draw by Rin Linin',
+        subTitle: 'watercolor',
+        description: 'I visit Team Lab at Japan and the flower room kind of made me want to draw this picture.'
+      },
+      previewPic: {
+        width: 'inherit',
+        height: 'inherit',
+        disabled: true
+      }
+    },
+    {
+      picUrl: '/assets/images/playground/etc/happy_bun_pizza_rin_linin.webp',
+      frameStyle: 'black-frame',
+      content: {
+        title: 'Rin Linin: Flower room',
+        title2: 'Design and draw by Rin Linin',
+        subTitle: 'watercolor',
+        description: 'I visit Team Lab at Japan and the flower room kind of made me want to draw this picture.'
+      },
+      previewPic: {
+        width: 'inherit',
+        height: 'inherit',
+        disabled: true
+      }
+    },
+    {
+      picUrl: '/assets/images/playground/etc/preist_jrpg_rin_linin.webp',
+      frameStyle: 'black-frame',
+      content: {
+        title: 'Rin Linin: Flower room',
+        title2: 'Design and draw by Rin Linin',
+        subTitle: 'watercolor',
+        description: 'I visit Team Lab at Japan and the flower room kind of made me want to draw this picture.'
+      },
+      previewPic: {
+        width: 'inherit',
+        height: 'inherit',
+        disabled: true
+      }
+    },
+    {
+      picUrl: '/assets/images/playground/etc/2023_gift_rin_linin.webp',
+      frameStyle: 'white-frame mini-frame-500',
+      content: {
+        title: 'Rin Linin: Flower room',
+        title2: 'Design and draw by Rin Linin',
+        subTitle: 'watercolor',
+        description: 'I visit Team Lab at Japan and the flower room kind of made me want to draw this picture.'
+      },
+      previewPic: {
+        width: 'inherit',
+        height: 'inherit',
+        disabled: true
+      }
+    },
+    {
+      picUrl: '/assets/images/playground/etc/rabbit_year_rin_linin.webp',
+      frameStyle: 'black-frame mb-auto circle small',
+      content: {
+        title: 'Rin Linin: Flower room',
+        title2: 'Design and draw by Rin Linin',
+        subTitle: 'watercolor',
+        description: 'I visit Team Lab at Japan and the flower room kind of made me want to draw this picture.'
+      },
+      previewPic: {
+        width: 'inherit',
+        height: 'inherit',
+        disabled: true
+      }
+    },
+  ];
+  etc3 = [
+    {
+      picUrl: '/assets/images/playground/etc/infp_rin_linin.webp',
+      frameStyle: 'black-frame',
+      content: {
+        title: 'Rin Linin: Flower room',
+        title2: 'Design and draw by Rin Linin',
+        subTitle: 'watercolor',
+        description: 'I visit Team Lab at Japan and the flower room kind of made me want to draw this picture.'
+      },
+      previewPic: {
+        width: 'inherit',
+        height: 'inherit',
+        disabled: true
+      }
+    },
+    {
+      picUrl: '/assets/images/playground/etc/chinese_novel_rin_linin.webp',
+      frameStyle: 'black-frame',
+      content: {
+        title: 'Rin Linin: Flower room',
+        title2: 'Design and draw by Rin Linin',
+        subTitle: 'watercolor',
+        description: 'I visit Team Lab at Japan and the flower room kind of made me want to draw this picture.'
+      },
+      previewPic: {
+        width: 'inherit',
+        height: 'inherit',
+        disabled: true
+      }
+    },
+    {
+      picUrl: '/assets/images/playground/etc/secret_garden_rin_linin.webp',
+      frameStyle: 'black-frame',
+      content: {
+        title: 'Rin Linin: Flower room',
+        title2: 'Design and draw by Rin Linin',
+        subTitle: 'watercolor',
+        description: 'I visit Team Lab at Japan and the flower room kind of made me want to draw this picture.'
+      },
+      previewPic: {
+        width: 'inherit',
+        height: 'inherit',
+        disabled: true
+      }
+    },
+
+  ];
 
 }
