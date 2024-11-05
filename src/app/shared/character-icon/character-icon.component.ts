e5d1ecea5ca3ad979b6080d9363c851af6a0eefc
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-11-06 00:30:26 +0700

    fix image icon always load

--- src/app/shared/character-icon/character-icon.component.ts
+++ src/app/shared/character-icon/character-icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MiniSpinningComponent } from "../mini-spinning/mini-spinning.component";
 
@@ -9,13 +9,20 @@ import { MiniSpinningComponent } from "../mini-spinning/mini-spinning.component"
   templateUrl: './character-icon.component.html',
   styleUrl: './character-icon.component.scss'
 })
-export class CharacterIconComponent {
+export class CharacterIconComponent implements OnInit {
   @Input() link: string;
   @Input() img: string;
   @Input() hoverDescription: string;
   @Input() label: string;
   @Input() fragment: string;
+  @ViewChild('imageIcon') imageIcon: ElementRef;
   imageLoading = true;
+
+  ngOnInit(): void {
+    if (this.imageIcon.nativeElement.complete) {
+      this.imageLoading = false;
+    }
+  }
 
   imageFinishLoading() {
     this.imageLoading = false;
