3e59492f6fbf6b79f5b9916ff2bef8d3fb4bddf4
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-11-07 23:18:56 +0700

    change to image html

--- src/app/shared/character-icon/character-icon.component.ts
+++ src/app/shared/character-icon/character-icon.component.ts
@@ -1,6 +1,5 @@
-import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { distinctUntilChanged, interval, Subject, Subscription, takeUntil } from 'rxjs';
 import { mainConfig } from '../../core/config/main.config';
 import { MiniSpinningComponent } from "../mini-spinning/mini-spinning.component";
 
@@ -11,7 +10,7 @@ import { MiniSpinningComponent } from "../mini-spinning/mini-spinning.component"
   templateUrl: './character-icon.component.html',
   styleUrl: './character-icon.component.scss'
 })
-export class CharacterIconComponent implements AfterViewInit, OnDestroy {
+export class CharacterIconComponent implements AfterViewInit {
   @Input() link: string;
   @Input() img: string;
   @Input() hoverDescription: string;
@@ -19,23 +18,9 @@ export class CharacterIconComponent implements AfterViewInit, OnDestroy {
   @Input() fragment: string;
   @ViewChild('imageIcon') imageIcon: ElementRef;
   imageLoading = true;
-  private subscription = new Subscription();
-  private completeLoad = new Subject();
 
   ngAfterViewInit(): void {
     setTimeout(() => {
-      // this,this.subscription.add(
-      //   interval(5000).pipe(
-      //     takeUntil(this.completeLoad.asObservable()),
-      //     distinctUntilChanged()
-      //   ).subscribe(
-      //     { next:(val) => {
-      //     if (this.imageIcon?.nativeElement.complete) {
-      //         this.imageLoading = false;
-      //         this.completeLoad.next(true);
-      //       }
-      //   }})
-      // );
       console.log('this.imageIcon?.nativeElement.complete', this.imageIcon?.nativeElement?.complete);
       console.log('this.imageIcon?.nativeElement.completeLoad', this.imageIcon?.nativeElement?.completeLoad);
       console.log('this.imageIcon?.nativeElement.naturalWidth', this.imageIcon?.nativeElement.naturalWidth);
@@ -45,10 +30,6 @@ export class CharacterIconComponent implements AfterViewInit, OnDestroy {
         // this.completeLoad.next(true);
       }
     }, mainConfig.timeoutAfterInit);
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
   }
 
   imageFinishLoading() {
