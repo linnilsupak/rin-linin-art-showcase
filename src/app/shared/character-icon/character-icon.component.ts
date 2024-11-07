c2b5efa11357e0931649f5709c196cafccec66cf
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-11-08 00:05:49 +0700

    remove unused

--- src/app/shared/character-icon/character-icon.component.ts
+++ src/app/shared/character-icon/character-icon.component.ts
@@ -21,13 +21,8 @@ export class CharacterIconComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     setTimeout(() => {
-      console.log('this.imageIcon?.nativeElement.complete', this.imageIcon?.nativeElement?.complete);
-      console.log('this.imageIcon?.nativeElement.completeLoad', this.imageIcon?.nativeElement?.completeLoad);
-      console.log('this.imageIcon?.nativeElement.naturalWidth', this.imageIcon?.nativeElement.naturalWidth);
-      console.log('this.imageIcon?.nativeElement.width', this.imageIcon?.nativeElement.width);
       if (this.imageIcon?.nativeElement.complete) {
         this.imageLoading = false;
-        // this.completeLoad.next(true);
       }
     }, mainConfig.timeoutAfterInit);
   }
