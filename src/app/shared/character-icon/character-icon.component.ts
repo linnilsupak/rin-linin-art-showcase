aed360d089614138112f86e6acb2fd8defa8fab7
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-11-07 22:55:21 +0700

    console.log

--- src/app/shared/character-icon/character-icon.component.ts
+++ src/app/shared/character-icon/character-icon.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { MiniSpinningComponent } from "../mini-spinning/mini-spinning.component";
+import { distinctUntilChanged, interval, Subject, Subscription, takeUntil } from 'rxjs';
 import { mainConfig } from '../../core/config/main.config';
+import { MiniSpinningComponent } from "../mini-spinning/mini-spinning.component";
 
 @Component({
   selector: 'app-character-icon',
@@ -10,7 +11,7 @@ import { mainConfig } from '../../core/config/main.config';
   templateUrl: './character-icon.component.html',
   styleUrl: './character-icon.component.scss'
 })
-export class CharacterIconComponent implements AfterViewInit {
+export class CharacterIconComponent implements AfterViewInit, OnDestroy {
   @Input() link: string;
   @Input() img: string;
   @Input() hoverDescription: string;
@@ -18,13 +19,33 @@ export class CharacterIconComponent implements AfterViewInit {
   @Input() fragment: string;
   @ViewChild('imageIcon') imageIcon: ElementRef;
   imageLoading = true;
+  private subscription = new Subscription();
+  private completeLoad = new Subject();
 
   ngAfterViewInit(): void {
     setTimeout(() => {
+      // this,this.subscription.add(
+      //   interval(5000).pipe(
+      //     takeUntil(this.completeLoad.asObservable()),
+      //     distinctUntilChanged()
+      //   ).subscribe(
+      //     { next:(val) => {
+      //     if (this.imageIcon?.nativeElement.complete) {
+      //         this.imageLoading = false;
+      //         this.completeLoad.next(true);
+      //       }
+      //   }})
+      // );
+      console.log('this.imageIcon?.nativeElement', this.imageIcon?.nativeElement);
       if (this.imageIcon?.nativeElement.complete) {
         this.imageLoading = false;
+        // this.completeLoad.next(true);
       }
     }, mainConfig.timeoutAfterInit);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   imageFinishLoading() {
