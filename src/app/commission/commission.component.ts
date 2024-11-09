9b3c5a07e3acbf83bdc8c0909263698373e95cf5
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-11-09 23:26:48 +0700

    tab style

--- src/app/commission/commission.component.ts
+++ src/app/commission/commission.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { ReflectionFontComponent } from "../shared/reflection-font/reflection-font.component";
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { ReflectionFontComponent } from "../shared/reflection-font/reflection-font.component";
 
 @Component({
   selector: 'app-commission',
   standalone: true,
-  imports: [ReflectionFontComponent, RouterLink, TranslateModule, RouterOutlet],
+  imports: [ReflectionFontComponent, RouterLink, TranslateModule],
   templateUrl: './commission.component.html',
   styleUrl: './commission.component.scss'
 })
