a72ce936c517e4e631dc592a4d33d36f19f883f6
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-11-10 10:37:06 +0700

    put version

--- src/app/core/http-loader.ts
+++ src/app/core/http-loader.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { environment } from '../../environments/environment';
+import { npm_package_version } from '../../environments/.env';
 
 
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  const version = environment.production ? npm_package_version: Date.now();
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json?v=' + encodeURIComponent(version));
 }
