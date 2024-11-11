f4d89adbd65a883ee5ecc6c4dae938adbe03efc7
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-11-11 17:07:46 +0700

    home commission section

--- src/app/commission/frame-item-animation/frame-item-animation.component.ts
+++ src/app/commission/frame-item-animation/frame-item-animation.component.ts
@@ -1,11 +1,12 @@
 import { AfterViewInit, Component, ElementRef, EventEmitter, HostListener, Input, Output, ViewChild } from '@angular/core';
 import { mainConfig } from '../../core/config/main.config';
 import { commissionTab } from '../../core/enum/commission-tab.enum';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-frame-item-animation',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './frame-item-animation.component.html',
   styleUrl: './frame-item-animation.component.scss'
 })
@@ -14,168 +15,85 @@ export class FrameItemAnimationComponent implements AfterViewInit {
   @Input()
   set activeVal(val: commissionTab) {
     if (this.activeItem === val) return;
-    if (val === commissionTab.FACE) {
-      this.topRightActive(true);
-    }
-    if (val === commissionTab.HALF) {
-      this.bottomRightActive(true);
-    }
-    if (val === commissionTab.FULL) {
-      this.centerActive(true);
-    }
-    if (val === commissionTab.DETAIL) {
-      this.rightActive(true);
-    }
-  }
-  @Input()
-  set isExample(val: boolean) {
-    this._isExample = val;
-  }
-  get isExample() {
-    return this._isExample;
+    let index = this.tabOrderList.findIndex((item) => item === val);
+    if (index === -1) index = 0;
+    this.itemActive(index, val, true);
   }
   @ViewChild('parentItem') parentItem: ElementRef<HTMLElement>;
   activeItem: commissionTab;
-  fullHeight = 900;
+  fullHeight = 800;
   fullWidth = 0;
   padding = 12;
-  halfHeight = 0;
   widthSection = 0;
+  loading = true;
 
   topRightHeight = 438;
-  rightWidth: number;
-  centerWidth: number;
-  leftWidth: number;
   bottomRightHeight = 438;
   commissionTab = commissionTab;
-  private _isExample = false;
   @HostListener('window:resize')
   public detectResize(): void {
+    this.loading = true;
     this.fullWidth = this.parentItem.nativeElement.offsetWidth;
-    this.halfHeight = (this.fullHeight / 2) - this.padding;
-    this.widthSection = (this.fullWidth - (this.padding * 2)) / 3;
-    this.rightWidth = this.widthSection;
-    this.centerWidth = this.widthSection;
-    this.leftWidth = this.widthSection;
+    this.widthSection = (this.fullWidth - (this.padding * 2)) / (this.tabOrderList.length);
+    this.tabOrderList.forEach((item, index) => {
+      this.tabList[index].width = this.widthSection;
+    });
+    this.loading = false;
   }
+  tabOrderList = [commissionTab.FACE, commissionTab.HALF, commissionTab.FULL, commissionTab.DETAIL];
+  tabList;
+  activeWidth = {
+    'FACE': 450,
+    'HALF': 450,
+    'FULL': 504,
+    'DETAIL': 480
+  };
 
   ngAfterViewInit(): void {
+    // init tabList
+    const list = [];
+    this.tabOrderList.forEach(tabName => {
+      list.push({
+        name: tabName,
+        activeWidth: this.activeWidth[tabName],
+        class: tabName.toLowerCase() + '-example',
+        width: ''
+      })
+    });
+    this.tabList = list;
     setTimeout(() => {
       this.detectResize();
     }, mainConfig.timeoutAfterInit);
   }
 
-  topRightActive(isClick = false) {
-    if (isClick && this.activeItem === commissionTab.FACE) {
+  itemActive(index: number, tab: commissionTab, isClick = false) {
+    console.log('tab', isClick, tab)
+    if (isClick && this.activeItem === tab) { // toggle
       this.setActiveTab(undefined);
       isClick = false;
     }
     if (isClick) {
-      this.resetState();
-      this.setActiveTab(commissionTab.FACE);
-      this.topRightHeight = 790;
-      this.leftWidth = 450;
-      this.bottomRightHeight = this.fullHeight - this.topRightHeight - this.padding;
+      this.resetStateActiveTab();
+      this.tabList[index].width = this.activeWidth[tab];
+      this.setActiveTab(tab);
     }
-    if (this.activeItem === undefined) {
-      if (!isClick) {
-        this.topRightHeight = 490;
-        this.leftWidth = this.widthSection;
-        this.bottomRightHeight = this.fullHeight - this.topRightHeight - this.padding;
-      }
+    if (this.activeItem === undefined && !isClick) {
+      this.tabList[index].width = this.widthSection + 20;
     }
   }
 
-  topRightInactive(forceInactive = false) {
+  itemInactive(tab: commissionTab, forceInactive = false) {
     if (this.activeItem === undefined || forceInactive) {
-      this.topRightHeight = this.halfHeight;
-      this.bottomRightHeight = this.halfHeight;
-      this.leftWidth = this.widthSection;
+      let index = this.tabOrderList.findIndex((item) => item === tab);
+      if (index === -1) index = 0;
+      this.tabList[index].width = this.widthSection;
     }
   }
 
-  bottomRightActive(isClick = false) {
-    if (isClick && this.activeItem === commissionTab.HALF) { // toggle
-      this.setActiveTab(undefined);
-      isClick = false;
-    }
-    if (isClick) {
-      this.resetState();
-      this.setActiveTab(commissionTab.HALF);
-      this.bottomRightHeight = 800;
-      this.leftWidth = 450;
-      this.topRightHeight = this.fullHeight - this.bottomRightHeight - this.padding;
-    }
-    if (this.activeItem === undefined) {
-      if (!isClick) {
-        this.bottomRightHeight = 490;
-        this.leftWidth = this.widthSection;
-        this.topRightHeight = this.fullHeight - this.bottomRightHeight - this.padding;
-      }
-    }
+  resetStateActiveTab() {
+    this.itemInactive(this.activeItem, true);
   }
 
-  bottomRightInactive(forceInactive = false) {
-    if (this.activeItem === undefined || forceInactive) {
-      this.bottomRightHeight = this.halfHeight;
-      this.topRightHeight = this.halfHeight;
-      this.leftWidth = this.widthSection;
-    }
-  }
-
-  centerActive(isClick = false) {
-    if (isClick && this.activeItem === commissionTab.FULL) { // toggle
-      this.setActiveTab(undefined);
-      isClick = false;
-    }
-    if (isClick) {
-      this.resetState();
-      this.setActiveTab(commissionTab.FULL);
-    }
-    if (this.activeItem === undefined || isClick) {
-      this.centerWidth = 620;
-    }
-  }
-  centerInactive(forceInactive = false) {
-    if (this.activeItem === undefined || forceInactive) {
-      this.centerWidth = this.widthSection;
-    }
-  }
-
-  rightActive(isClick = false) {
-    if (isClick && this.activeItem === commissionTab.DETAIL) { // toggle
-      this.setActiveTab(undefined);
-      isClick = false;
-    }
-    if (isClick) {
-      this.resetState();
-      this.setActiveTab(commissionTab.DETAIL);
-    }
-    if (this.activeItem === undefined || isClick) {
-      this.rightWidth = 510;
-    }
-  }
-
-  rightInactive(forceInactive = false) {
-    if (this.activeItem === undefined || forceInactive) {
-      this.rightWidth = this.widthSection;
-    }
-  }
-
-  resetState() {
-    if (this.activeItem === commissionTab.FACE) {
-      this.topRightInactive(true);
-    }
-    if (this.activeItem === commissionTab.HALF) {
-      this.bottomRightInactive(true);
-    }
-    if (this.activeItem === commissionTab.FULL) {
-      this.centerInactive(true);
-    }
-    if (this.activeItem === commissionTab.DETAIL) {
-      this.rightInactive(true);
-    }
-  }
 
   setActiveTab(val: commissionTab) {
     this.activeItem = val;
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,15 @@ import { ImageCardComponent } from "../shared/image-card/image-card.component";
 import { CharacterIconComponent } from "../shared/character-icon/character-icon.component";
 import { AboutMePicHeaderComponent } from "../about-me/about-me-pic-header/about-me-pic-header.component";
 import { SocialMediaSectionComponent } from "../shared/social-media-section/social-media-section.component";
+import { ImageSrcsetPipe } from "../core/pipe/image-srcset.pipe";
+import { PlaygroundMenuComponent } from "../playground/playground-menu/playground-menu.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TranslateModule, ReflectionFontComponent, ImageCardComponent, CharacterIconComponent, AboutMePicHeaderComponent, SocialMediaSectionComponent],
+  imports: [TranslateModule, ReflectionFontComponent, ImageCardComponent, RouterLink,
+     CharacterIconComponent, AboutMePicHeaderComponent, SocialMediaSectionComponent, ImageSrcsetPipe, PlaygroundMenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
