eba59d431603c842df4fc7cc7b3ed4eca4f3dec5
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-12-02 22:39:33 +0700

    add prod env

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  contactMeForm: 'https://docs.google.com/forms/d/e/1FAIpQLSeBRCEJaE1ryzuUW1B7bTZf8ZUiY_o4kvFJfU94xTzI1kxGQg',
+  typeCommissionForm: {
+    face: 'https://docs.google.com/forms/d/e/1FAIpQLSeLwTzeol8ICdU5hzcqn1cfycP1u4dxf07Md0cF-Fpq9bRn3A',
+    half: 'https://docs.google.com/forms/d/e/1FAIpQLScYSJVkn2PvciPXHOgGvAZGj1OLx1mNXaKw9YG7UhBkaoO4FQ',
+    full: 'https://docs.google.com/forms/d/e/1FAIpQLSdPZVfl56UwMad69Wpmfq1LNoAm_qSZ7U7nrUip1G3qL5Jlkw',
+    detail: 'https://docs.google.com/forms/d/e/1FAIpQLSdCybb1Jsr_DZ8m1hZrXhwdXqLbklxyMqUr3UuKZpIyV7wtcQ'
+  }
+};
