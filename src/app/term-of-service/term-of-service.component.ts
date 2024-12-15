153c38d21a26a1301a82c10b99db2da2e0d14ef8
linnil <39083566+linnilsupak@users.noreply.github.com>
2024-12-15 05:41:54 +0000

    change TOS

--- src/app/term-of-service/term-of-service.component.ts
+++ src/app/term-of-service/term-of-service.component.ts
@@ -14,7 +14,14 @@ import { CommonModule } from '@angular/common';
 export class TermOfServiceComponent {
 backUrl: any;
 characterData: any;
-generalRule = new Array(15);
-commercial = new Array(2);
+generalRule = new Array(5);
+payment = new Array(3);
+process = new Array(4);
+revision = new Array(3)
+copyRight1 = new Array(6);
+copyRight2 = new Array(6)
+copyRight3 = new Array(3);
+commercial = new Array(3);
+refund = new Array(5);
 
 }
